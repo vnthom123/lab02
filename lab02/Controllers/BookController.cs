681dc95cdf9f7dd2585473d78ef3c1fc90acdc01
Thom Tran <com>
2021-06-24 15:15:45 +0700

    new commit

---
+++ lab02/Controllers/BookController.cs
@@ -0,0 +1,152 @@
+﻿using lab02.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lab02.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        public ActionResult Index()
+        {
+            return View();
+        }
+        /*public ActionResult ListBook()
+        {
+            var books = new List<string>();
+            books.Add("HTML5 & CSS3 The complete Manual-Author name book 1");
+            books.Add("HTML5 & CSS Responsive web Design cookbook-Author name book 2");
+            books.Add("Professional ASP.NET MVC5-Author name book 3");
+            ViewBag.Books = books;
+            return View();
+        }*/
+        public ActionResult ListBookModel()
+        {
+            var books = new List<Book>();
+            books.Add(new Book(1,"HTML5 & CSS3 The complete Manual","Author name book 1","/content/Images/book1cover.png"));
+            books.Add(new Book(2,"HTML5 & CSS Responsive web Design cookbook","Author name book 2", "/content/Images/book2cover.png"));
+            books.Add(new Book(3,"Professional ASP.NET MVC5","Author name book 3", "/content/Images/book3cover.png"));
+            //ViewBag.Books = books;
+            return View(books);
+        }
+        public ActionResult EditBook(int id)
+        {
+            var books = new List<Book>();
+            books.Add(new Book(1, "HTML5 & CSS3 The complete Manual", "Author name book 1", "/content/Images/book1cover.png"));
+            books.Add(new Book(2, "HTML5 & CSS Responsive web Design cookbook", "Author name book 2", "/content/Images/book2cover.png"));
+            books.Add(new Book(3, "Professional ASP.NET MVC5", "Author name book 3", "/content/Images/book3cover.png"));
+            Book book = new Book();
+            foreach(Book b in books)
+            {
+                if (b.Id==id)
+                {
+                    book = b;
+                    break;
+                }               
+            }
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);
+        }
+        [HttpPost,ActionName("EditBook")]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditBook(int id,string title, string author, string image_cover)
+        {
+            var books = new List<Book>();
+            books.Add(new Book(1, "HTML5 & CSS3 The complete Manual", "Author name book 1", "/content/Images/book1cover.png"));
+            books.Add(new Book(2, "HTML5 & CSS Responsive web Design cookbook", "Author name book 2", "/content/Images/book2cover.png"));
+            books.Add(new Book(3, "Professional ASP.NET MVC5", "Author name book 3", "/content/Images/book3cover.png"));
+            Book book = new Book();
+            foreach (Book b in books)
+            {
+                if (b.Id == id)
+                {
+                    b.Title = title;
+                    b.Author = author;
+                    b.Image_cover = image_cover;
+                    break;
+                }
+            }
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            return View("ListBookModel",books);
+        }
+        public ActionResult CreateBook()
+        {
+            return View();
+        }
+        [HttpPost, ActionName("createBook")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Contact([Bind(Include="Id, Title, Author, ImageCover")] Book book)
+        {
+            var books = new List<Book>();
+            books.Add(new Book(1, "HTML5 & CSS3 The complete Manual", "Author name book 1", "/content/Images/book1cover.png"));
+            books.Add(new Book(2, "HTML5 & CSS Responsive web Design cookbook", "Author name book 2", "/content/Images/book2cover.png"));
+            books.Add(new Book(3, "Professional ASP.NET MVC5", "Author name book 3", "/content/Images/book3cover.png"));
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    books.Add(book);
+                }
+            }
+            catch (Exception)//RetryLimitExceededException
+            {
+                ModelState.AddModelError("", "Eror save data");
+            }
+            return View("ListBookModel", books);
+        }
+        public ActionResult DeleteBook(int id)
+        {
+            var books = new List<Book>();
+            books.Add(new Book(1, "HTML5 & CSS3 The complete Manual", "Author name book 1", "/content/Images/book1cover.png"));
+            books.Add(new Book(2, "HTML5 & CSS Responsive web Design cookbook", "Author name book 2", "/content/Images/book2cover.png"));
+            books.Add(new Book(3, "Professional ASP.NET MVC5", "Author name book 3", "/content/Images/book3cover.png"));
+            Book book = new Book();
+            foreach (Book b in books)
+            {
+                if (b.Id == id)
+                {
+                    book = b;
+                    break;
+                }
+            }
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);
+        }
+        [HttpPost, ActionName("DeleteBook")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            var books = new List<Book>();
+            books.Add(new Book(1, "HTML5 & CSS3 The complete Manual", "Author name book 1", "/content/Images/book1cover.png"));
+            books.Add(new Book(2, "HTML5 & CSS Responsive web Design cookbook", "Author name book 2", "/content/Images/book2cover.png"));
+            books.Add(new Book(3, "Professional ASP.NET MVC5", "Author name book 3", "/content/Images/book3cover.png"));
+            Book book = new Book();
+            foreach (Book b in books)
+            {
+                if (b.Id == id)
+                {
+                    books.Remove(b);
+                    break;
+                }
+            }
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            return View("ListBookModel", books);
+        }
+    }
+
+    }
---
+++ lab02/Models/Book.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace lab02.Models
+{
+    public class Book
+    {
+        private int id;
+        private string title;
+        private string author;
+        private string image_cover;
+        public Book() { }
+        public int Id { get { return id; } }
+        [Required(ErrorMessage ="Tieu de khong duoc de trong")]
+        [StringLength(250,ErrorMessage ="Tieu de sach khong duoc vuot qua 250 ky tu")]
+        [Display(Name ="Tieu de")]
+        public string Title { get => title; set => title = value; }
+        public string Author { get => author; set => author = value; }
+        public string Image_cover { get => image_cover; set => image_cover = value; }
+
+        public Book(int id, string title, string author, string image_cover)
+        {
+            this.id = id;
+            this.title = title;
+            this.author = author;
+            this.image_cover = image_cover;
+        }
+    }
+}
